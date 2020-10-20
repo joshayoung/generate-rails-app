7451aead6274e94a0f1ab27502045fc8cd08971c
Josh Young <josh.young@daveramsey.com>
2020-10-20 14:17:32 -0500

    Improve on script for generating rails repo

--- generate.sh
+++ generate.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 docker build -t docker-rails-generator .
-NEW_APP_PATH=$PWD
-NEW_RAILS_APP=myrailsapp
+
+read -p "Enter path for newly generated project [$PWD]: " NEW_APP_PATH
+NEW_APP_PATH=${NEW_APP_PATH:-$PWD}
+
+read -p "Enter name for your new app [myrailsapp]: " NEW_RAILS_APP
+NEW_RAILS_APP=${NEW_RAILS_APP:-myrailsapp}
+
 NEW_APP_COMPLETE_PATH=${NEW_APP_PATH}/${NEW_RAILS_APP}
 
 docker run -it -v $NEW_APP_PATH:/app docker-rails-generator rails new --skip-bundle $NEW_RAILS_APP
 cp -r ./docker-dev-files/* $NEW_APP_COMPLETE_PATH
-cd myrailsapp
+cd $NEW_APP_COMPLETE_PATH 
 docker-compose build
 docker-compose up -d
 docker exec -it ${NEW_RAILS_APP}_dev_1 /bin/bash
